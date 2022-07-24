45ec66e98dcc177d72b572caf375c9775c227124
MarinoLinic <84214511+MarinoLinic@users.noreply.github.com>
2022-07-24 22:33:20 +0200

    improvements

--- main.py
+++ main.py
@@ -17,100 +17,80 @@ class Colors:
     BOSS = [120, 0, 0, 255]
 
 
+def filling_pixels(fill_color):
+    for k in range(increment):
+        for h in range(increment):
+            b[i+k][j+h] = fill_color
+
+
 def main():
     filename = "Level1.png"
-
     image = PIL.Image.open(filename)
 
     size = width, height = image.size
+    # print("Shape of the array is : ", np.shape(a))
 
-    print(size)
-
-    colors = image.getcolors(maxcolors=256) # how many pixels use what color
-
+    # print(size)
+    # colors = image.getcolors(maxcolors=256) # how many pixels use what color
+    # print(colors)
+    # print(PIL.features.pilinfo())
     # print(list(image.getdata()))
-
+    # print(a)
+    # print("something:", len(b[1079]))
+    # fill_color = 0, 255, 255, 255
 
     a = np.asarray(image)
 
-    print("Shape of the array is : ", np.shape(a))
-
-    # print(PIL.features.pilinfo())
-
-    # print(list(image.getdata()))
-
-    # print(a)
-    print(colors)
-
-    b = a.copy()
-
-    b.setflags(write=1) # making array writable
-
-
-    print("something:", len(b[1079]))
-
-    f = open("coordinates.txt", "w")
-
+    global i
+    global j
+    global increment
     i = 0
     j = 0
-    increment = 50
+    increment = 1
 
-    # for i in range(1, len(b)-60, increment):
-    while i < len(b)-increment:
+    global b
+    b = a.copy()
+    b.setflags(write=1) # making array writable
+
+    f = open("coordinates.txt", "w")
+    while i < len(b)-(increment-1):
         j = 0
-        # for j in range(1, len(b[i])-60, increment):
-        while j < len(b[i])-increment:
-            # value = b[i][j] == [0, 0, 0, 255]
-            # print(type(value))
+        while j < len(b[i])-(increment-1):
+            # value = b[i][j] == [0, 0, 0, 255] = why did this work?
             if (np.asarray(Colors.BLACK) == b[i][j]).all():
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 255, 255, 255, 255
+                fill_color = 255, 255, 255, 255
 
             elif (np.asarray(Colors.PLATFORM) == b[i][j]).all():
-                print("test")
                 f.write("{} - X: {}, Y: {}{}".format("Platform", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = b[i][j] = 0, 255, 255, 255
+                fill_color = 0, 255, 255, 255
 
             elif (np.asarray(Colors.PLATFORM2) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("PlatformObject", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 255, 0, 255, 255
+                fill_color = 255, 0, 255, 255
 
             elif (np.asarray(Colors.PLATFORM3) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("PlatformObject2", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 255, 255, 0, 255
+                fill_color = 255, 255, 0, 255
 
             elif (np.asarray(Colors.WALL) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("PlatformWall", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 255, 0, 0, 255
+                fill_color = 255, 0, 0, 255
 
             elif (np.asarray(Colors.PLAYER) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("Player", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 0, 255, 0, 255
+                fill_color = 0, 255, 0, 255
 
             elif (np.asarray(Colors.ENEMY) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("Enemy", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 0, 0, 255, 255
+                fill_color = 0, 0, 255, 255
 
             elif (np.asarray(Colors.BOSS) == b[i][j]).all():
                 f.write("{} - X: {}, Y: {}{}".format("Boss", j, i, "\n"))
-                for k in range(increment):
-                    for h in range(increment):
-                        b[i+k][j+h] = 0, 0, 0, 255
+                fill_color = 0, 0, 0, 255
 
+            filling_pixels(fill_color)
             j += increment
+
         i += increment
 
     f.close()
