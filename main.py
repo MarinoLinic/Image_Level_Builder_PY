25027d0da303236f05994ff3cba68b5cb13a088a
Marino Linić <84214511+MarinoLinic@users.noreply.github.com>
2022-07-25 12:57:40 +0200

    Added a few classes

--- main.py
+++ main.py
@@ -4,6 +4,50 @@ import numpy as np
 import os
 
 
+class Text_Variables:
+    def __init__(self, x, y, w, h):
+        PLATFORM = "Platform.generateRectangle({{x: {},\ty: {},\tw: {},\th: {},\t"
+        "sprite: {{default: `${{PATH_SPRITES}}/Level 2/Platform.png`}}}});".format(x, y, w, h)
+
+        ENEMY_MELEE = "{{x: {}, y: {}, w: {}, h: {}, jumpHeight: 0, jumps: 0, movespeed: 3, seekingMovespeedFactor: 1.5, "
+        "HP: 50, contactDamage: 15, patrolDistance: 500, attackCooldown: 3000}},".format(x, y, w, h)
+
+        ENEMY_RANGED = "{{x: {}, y: {}, w: {}, h: {}, jumpHeight: 0, jumps: 0, movespeed: 0, HP: 30, contactDamage: 5, patrolDistance: 1500, attackCooldown: 1000, weapon: new Weapon({{"
+        "\n\tname: \"Izazgrabi\",\n"
+        "\n\tdamage: 10,"
+        "\n\tsound: createSound(`${PATH_AUDIO}/Weapons/Ranged/Izazgrabi/Fire.mp3`),"
+        "\n\tmissileSpeed: 7,"
+        "\n\tmissileSize: {w: 30, h: 15},"
+        "\n\tfireEffectSize: {w: 64, h: 64},"
+        "\n\tmissileSprite: {"
+        "\n\t\tleft: `${PATH_SPRITES}/Weapons/Ranged/Izazgrabi/MissileLeft.png`,"
+        "\n\t\tright: `${PATH_SPRITES}/Weapons/Ranged/Izazgrabi/MissileRight.png`,"
+        "\n\t\tup: `${PATH_SPRITES}/Weapons/Ranged/Izazgrabi/MissileUp.png`,"
+        "\n\t\tfire: {"
+        "\n\t\t\tleft: `${PATH_SPRITES}/Weapons/Ranged/Izazgrabi/FiredLeft.png`,"
+        "\n\t\t\tright: `${PATH_SPRITES}/Weapons/Ranged/Izazgrabi/FiredRight.png`,"
+        "\n\t\t}"
+        "\n\t},"
+        "\n})},".format(x, y, w, h)
+
+        PLAYER = "\tPLAYER = new PlayerCharacter({\r\n\t\tx: {},\r\n\t\ty: {},\r\n\t\tw: {},\r\n\t\th: {},\r\n\t\tcrouchHeight: 62,\r\n\t\tjumpHeight: 15,\r\n\t\tjumps: 1,\r\n\t\tmovespeed: 5,\r\n\t\tHP: 100.0,\r\n\t\tsprite: {\r\n\t\t\tidle: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Player/PlayerIdleLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Player/PlayerIdleRight.png`,\r\n\t\t\t},\r\n\t\t\tmove: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Player/PlayerMoveLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Player/PlayerMoveRight.png`,\r\n\t\t\t},\r\n\t\t\tcrouch: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Player/PlayerCrouchLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Player/PlayerCrouchRight.png`,\r\n\t\t\t},\r\n\t\t\tjump: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Player/PlayerJumpLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Player/PlayerJumpRight.png`,\r\n\t\t\t},\r\n\t\t\tfall: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Player/PlayerFallLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Player/PlayerFallRight.png`,\r\n\t\t\t},\r\n\t\t\tdefault: `${PATH_SPRITES}/Player/PlayerIdleRight.png`,\r\n\t\t},\r\n\t\tplayerName: \"Pobar\",\r\n\t\tweapon: new Weapon({\r\n\t\t\tname: \"Izanagi\",\r\n\t\t\tdamage: 10,\r\n\t\t\tsound: createSound(`${PATH_AUDIO}/Weapons/Ranged/Izanagi/Fire.mp3`),\r\n\t\t\tmissileSpeed: 20,\r\n\t\t\tmissileSize: {w: 21, h: 10},\r\n\t\t\tfireEffectSize: {w: 64, h: 64},\r\n\t\t\tmissileSprite: {\r\n\t\t\t\tleft: `${PATH_SPRITES}/Weapons/Ranged/Izanagi/MissileLeft.png`,\r\n\t\t\t\tright: `${PATH_SPRITES}/Weapons/Ranged/Izanagi/MissileRight.png`,\r\n\t\t\t\tup: `${PATH_SPRITES}/Weapons/Ranged/Izanagi/MissileUp.png`,\r\n\t\t\t\tfire: {\r\n\t\t\t\t\tleft: `${PATH_SPRITES}/Weapons/Ranged/Izanagi/FiredLeft.png`,\t\r\n\t\t\t\t\tright: `${PATH_SPRITES}/Weapons/Ranged/Izanagi/FiredRight.png`,\r\n\t\t\t\t},\r\n\t\t\t},\r\n\t\t}),\r\n\t});".format(x, y, w, h)
+
+
+class Text_Paste:
+    def __init__(self, text):
+        self.text = text
+
+
+class Pixel_Object:
+    def __init__(self, file_name, color, text):
+        self.file_name = file_name
+        self.texture = PIL.Image.open(file_name)
+        self.size = width, height = texture.size
+        self.color = color
+        self.inverted_color = invert_color(color)
+        self.text = text
+
+
 class Colors:
     WHITE = [255, 255, 255, 255]
     BLACK = [0, 0, 0, 255]
